3cbd9f3684e493c80ced7499f3b0252d45f6f1aa
Miracle <miracleama17@gmail.com>
2026-04-28 20:14:48 +0100

    Added updateCandidate and deleteCandidate routes

--- src/routes/admin.candidate.routes.js
+++ src/routes/admin.candidate.routes.js
@@ -3,9 +3,18 @@ const router = express.Router();
 import {
   getCandidates,
   createCandidate,
+  updateCandidate,
+  deleteCandidate,
 } from "../controllers/admin.candidate.controller.js";
 
+router
+  .route("/")
+  .get(getCandidates)
+  .post(createCandidate)
 
-router.route("/").get(getCandidates).post(createCandidate);
+router
+  .route("/:id")
+  .patch(updateCandidate)
+  .delete(deleteCandidate); 
 
-export default router;  
+export default router;
